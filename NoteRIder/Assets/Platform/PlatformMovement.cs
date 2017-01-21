fdeced5e5ada46f30af73ebd33b6623dad4e4a35
Ben Nguyen <thiennguyen12313@gmail.com>
2017-01-21 12:45:02 -0600

    Update

--- NoteRIder/Assets/Platform/PlatformMovement.cs
+++ NoteRIder/Assets/Platform/PlatformMovement.cs
@@ -5,29 +5,27 @@ public class PlatformMovement: MonoBehaviour {
     public GameObject thePlatform;
     public Transform generationPoint;
     public float distanceBetween;
-    public Transform farEnd;
-    private Vector3 frometh;
-    private Vector3 untoeth;
-    private float secondsForOneLength = 20f;
+   // public Transform farEnd;
+   // private Vector3 frometh;
+    //private Vector3 untoeth;
+    //private float secondsForOneLength = 20f;
 
     private float platformWidth;
 
     void Start() {
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
-        frometh = transform.position;
-        untoeth = farEnd.position;
+        //frometh = transform.position;
+        //untoeth = farEnd.position;
     }
 
     void Update() {
         
-         if (transform.position.x >= generationPoint.position.x - platformWidth)
+         if (transform.position.x < generationPoint.position.x)
         {
-            while (true)
-            {
+    
                 transform.position = new Vector2(transform.position.x + platformWidth + distanceBetween, transform.position.y);
                 Instantiate(thePlatform, transform.position, transform.rotation);
-            }
         }
-        transform.position = Vector3.Lerp(frometh, untoeth, Mathf.SmoothStep(0f, 1f, Mathf.PingPong(Time.time / secondsForOneLength, 1f)));
+        //transform.position = Vector3.Lerp(frometh, untoeth, Mathf.SmoothStep(0f, 1f, Mathf.PingPong(Time.time / secondsForOneLength, 1f)));
     }
 }
---
+++ NoteRIder/Assets/platf.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platf : MonoBehaviour {
+    private Rigidbody2D body;
+    public float moveRight;
+	// Use this for initialization
+	void Start () {
+        body = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 vec = new Vector2(moveRight, 0f);
+        body.AddForce(vec * 10);
+    }
+}
