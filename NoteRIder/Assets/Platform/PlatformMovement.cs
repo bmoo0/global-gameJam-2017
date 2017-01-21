587f40c93ac561941fba510068207004231da1ad
Ben Nguyen <thiennguyen12313@gmail.com>
2017-01-21 11:29:26 -0600

    update

--- NoteRIder/Assets/Platform/PlatformMovement.cs
+++ NoteRIder/Assets/Platform/PlatformMovement.cs
@@ -5,12 +5,17 @@ public class PlatformMovement: MonoBehaviour {
     public GameObject thePlatform;
     public Transform generationPoint;
     public float distanceBetween;
-    public double y;
+    public Transform farEnd;
+    private Vector3 frometh;
+    private Vector3 untoeth;
+    private float secondsForOneLength = 20f;
 
     private float platformWidth;
 
     void Start() {
         platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+        frometh = transform.position;
+        untoeth = farEnd.position;
     }
 
     void Update() {
@@ -19,10 +24,10 @@ public class PlatformMovement: MonoBehaviour {
         {
             while (true)
             {
-                y += 1.5;
                 transform.position = new Vector2(transform.position.x + platformWidth + distanceBetween, transform.position.y);
                 Instantiate(thePlatform, transform.position, transform.rotation);
             }
         }
+        transform.position = Vector3.Lerp(frometh, untoeth, Mathf.SmoothStep(0f, 1f, Mathf.PingPong(Time.time / secondsForOneLength, 1f)));
     }
 }
