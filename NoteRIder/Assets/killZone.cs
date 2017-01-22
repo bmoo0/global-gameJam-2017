c2b5f6d74b96cbf07fbbebd70fc30338e55dc46b
Ben Moore <benmoore422@gmail.com>
2017-01-21 20:08:06 -0600

    added killzone

--- NoteRIder/Assets/killZone.cs
+++ NoteRIder/Assets/killZone.cs
@@ -5,17 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class killZone : MonoBehaviour {
 
+	Transform ball;
+
 	// Use this for initialization
 	void Start () {
-		
+		ball = GameObject.Find("Circle").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = new Vector3 (ball.position.x, transform.position.y, transform.position.z);
 	}
 
-	void OnTriggerEnter2D (Collider2D col) {
+	void OnTriggerEnter2D(Collider2D col) {
 		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
 	}
 }
