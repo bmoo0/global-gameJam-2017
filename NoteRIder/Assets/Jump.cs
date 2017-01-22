5ea51dc2d2008cf7944b7fdc90778c61461217a5
Ben Nguyen <thiennguyen12313@gmail.com>
2017-01-21 19:53:58 -0600

    Double jump bug fix

--- NoteRIder/Assets/Jump.cs
+++ NoteRIder/Assets/Jump.cs
@@ -28,7 +28,7 @@ public class Jump : MonoBehaviour {
 			canJump = true;
 		}
 
-		if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetMouseButtonDown(0) && this.canJump == true) {
+		if (Input.GetKeyDown (KeyCode.UpArrow) && this.canJump == true) {
 			Vector2 vec = new Vector2 (12f, jump);
 			body.AddForce (vec * 10);
             playerJump.Play();
